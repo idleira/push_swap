0cd1abd1ca115643cfd8d4c2e00c1a9dca901d5d
ibeliaie <ibeliaie@student.42wolfsburg.de>
2023-07-24 19:00:53 +0200

    new stack, add bottom element and get size :O

--- stack_manipulations.c
+++ stack_manipulations.c
@@ -6,7 +6,7 @@
 /*   By: ibeliaie <ibeliaie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 22:40:52 by ibeliaie          #+#    #+#             */
-/*   Updated: 2023/07/20 22:53:51 by ibeliaie         ###   ########.fr       */
+/*   Updated: 2023/07/24 18:16:50 by ibeliaie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,54 @@ t_stack	*get_second_to_last(t_stack *stack)
 	while (stack && stack->next && stack->next->next != NULL)
 		stack = stack->next;
 	return (stack);
+}
+
+/* create a stack elements with the provided value */
+t_stack	*new_stack(int value)
+{
+	t_stack	*new;
+
+	new = malloc(sizeof * new);
+	if (!new)
+		return (NULL);
+	new->value = value;
+	new->index = 0;
+	new->pos = -1;
+	new->target_pos = -1;
+	new->cost_a = -1;
+	new->cost_b = -1;
+	new->next = NULL;
+	return (new);
+}
+
+/* add an element to the bottom of a stack */
+void	add_bottom(t_stack **stack, t_stack *new)
+{
+	t_stack	*tail;
+
+	if (!new)
+		return ;
+	if (!*stack)
+	{
+		*stack = new;
+		return ;
+	}
+	tail = get_last_element(*stack);
+	tail->next = new;
+}
+
+/* return size of the stack */
+int	stack_size(t_stack	*stack)
+{
+	int	size;
+
+	size = 0;
+	if (!stack)
+		return (0);
+	while (stack)
+	{
+		stack = stack->next;
+		size++;
+	}
+	return (size);
 }
