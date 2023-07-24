a03a4d35fb9abb7aec04ecf481c29db801972224
ibeliaie <ibeliaie@student.42wolfsburg.de>
2023-07-24 18:56:05 +0200

    omg it checks input and has utils :o

---
+++ check_input.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_input.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibeliaie <ibeliaie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/24 18:05:54 by ibeliaie          #+#    #+#             */
+/*   Updated: 2023/07/24 18:33:09 by ibeliaie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static int	duplicates(char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (argv[i])
+	{
+		j = 1;
+		while (argv[j])
+		{
+			if (j != i && nbstr_cmp(argv[i], argv[j]) == 0)
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+static int	arg_is_zero(char *argv)
+{
+	int	i;
+
+	i = 0;
+	if (is_sign(argv[i]))
+		i++;
+	while (argv[i] && argv[i] == '0')
+		i++;
+	if (argv[i] != '\0')
+		return (0);
+	return (1);
+}
+
+static int	arg_is_number(char *argv)
+{
+	int	i;
+
+	i = 0;
+	if (is_sign(argv[i]) && argv[i + 1] != '\0')
+		i++;
+	while (argv[i] && is_digit(argv[i]))
+		i++;
+	if (argv[i] != '\0' && !is_digit(argv[i]))
+		return (0);
+	return (1);
+}
+
+/* check if arguments are all numbers, without duplicates */
+int	correct_input(char **argv)
+{
+	int	i;
+	int	nb_zeros;
+
+	nb_zeros = 0;
+	i = 1;
+	while (argv[i])
+	{
+		if (!arg_is_number(argv[i]))
+			return (0);
+		nb_zeros += arg_is_zero(argv[i]);
+		i++;
+	}
+	if (nb_zeros > 1)
+		return (0);
+	if (hargve_duplicates(argv))
+		return (0);
+	return (1);
+}
---
+++ check_input_util.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_input_util.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibeliaie <ibeliaie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/24 18:05:57 by ibeliaie          #+#    #+#             */
+/*   Updated: 2023/07/24 18:29:32 by ibeliaie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	is_sign(char c)
+{
+	return (c == '+' || c == '-');
+}
+
+int	is_digit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+/* compare 2 strings of digits to check if they are the same or not */
+int	compare_nbr_str(const char *s1, const char *s2)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = i;
+	if (s1[i] == '+')
+	{
+		if (s2[j] != '+')
+			i++;
+	}
+	else
+	{
+		if (s2[j] == '+')
+			j++;
+	}
+	while (s1[i] != '\0' && s2[j] != '\0' && s1[i] == s2[j])
+	{
+		i++;
+		j++;
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[j]);
+}
