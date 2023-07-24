676b51853f16678e1287e165b131e7d937d4b6b2
ibeliaie <ibeliaie@student.42wolfsburg.de>
2023-07-24 19:01:23 +0200

    util go mental, so do i :O

--- util.c
+++ util.c
@@ -6,13 +6,65 @@
 /*   By: ibeliaie <ibeliaie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 22:56:51 by ibeliaie          #+#    #+#             */
-/*   Updated: 2023/07/20 22:57:57 by ibeliaie         ###   ########.fr       */
+/*   Updated: 2023/07/24 18:46:45 by ibeliaie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-/* print string to standart output */
+/* free each element in a given stack and set the stack pointer to NULL */
+void	free_stack(t_stack **stack)
+{
+	t_stack	*tmp;
+
+	if (!stack || !(*stack))
+		return ;
+	while (*stack)
+	{
+		tmp = (*stack)->next;
+		free(*stack);
+		*stack = tmp;
+	}
+	*stack = NULL;
+}
+
+/* convert string to long int */
+long int	ft_atoi(const char *str)
+{
+	long int	nb;
+	int			isneg;
+	int			i;
+
+	nb = 0;
+	isneg = 1;
+	i = 0;
+	if (str[i] == '+')
+		i++;
+	else if (str[i] == '-')
+	{
+		isneg *= -1;
+		i++;
+	}
+	while (is_digit(str[i]))
+	{
+		nb = (nb * 10) + (str[i] - '0');
+		i++;
+	}
+	return (nb * isneg);
+}
+
+/* write error to the standard output after freeing */
+void	error_exit(t_stack **stack_a, t_stack **stack_b)
+{
+	if (stack_a == NULL || *stack_a != NULL)
+		free_stack(stack_a);
+	if (stack_b == NULL || *stack_b != NULL)
+		free_stack(stack_b);
+	write(2, "Error\n", 6);
+	exit (1);
+}
+
+/* print given string to standart output */
 void	ft_putstr(char *str)
 {
 	int	i;
@@ -23,4 +75,12 @@ void	ft_putstr(char *str)
 		write(1, &str[i], 1);
 		i++;
 	}
+}
+
+/* return absolute value of a number */
+int	absolute_val(int nb)
+{
+	if (nb < 0)
+		return (nb * -1);
+	return (nb);
 }
